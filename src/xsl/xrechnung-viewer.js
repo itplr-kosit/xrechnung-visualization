938a5b30aafb83176e6cc03a80b98397687da049
lukas.kumai <l.kumai@lava-unternehmensberatung.de>
2021-11-09 13:14:52 +0100

    fixed safari issue

--- src/xsl/xrechnung-viewer.js
+++ src/xsl/xrechnung-viewer.js
@@ -67,16 +67,8 @@ function downloadData(element_id) {
         window.navigator.msSaveOrOpenBlob(blob, filename);
     } else {
         // Non-IE
-        var a = document.createElement('a');
-        document.body.appendChild(a);
-        const url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = filename;
-        a.click();
-        setTimeout(() => {
-            window.URL.revokeObjectURL(url);
-            document.body.removeChild(a);
-        }, 0)
+        var url = window.URL.createObjectURL(blob);
+        window.open(url, "_self");
     }
 }
 
